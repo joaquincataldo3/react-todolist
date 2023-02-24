357a1a8a402874532802504b238f8e9b8fe46d37
joaquincataldo3 <joaco.cataldo3@gmail.com>
2023-02-24 12:35:00 -0300

    organizing routes and styling not found

---
+++ src/Comps/NotFound.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './NotFound.css'
+import { Link } from 'react-router-dom'
+
+function NotFound() {
+  return (
+    <main className='not-found-container'>
+       <h1 className='not-found-title'>The page you are trying to look for, doesn't exist</h1>
+       <Link to='/' className='back-home-anchor'>
+       <h3 className='not-found-subtitle'>Go back home</h3>
+       </Link>
+    </main>
+  )
+}
+
+export default NotFound
--- src/index.js
+++ src/index.js
@@ -3,11 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import NotFound from './Comps/NotFound';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
